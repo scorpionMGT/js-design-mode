a80758f0fe0ef5215bf0a74fbc9f79c6ee93d9bf
maoguotao <1142656486@qq.com>
2018-10-05 23:41:18 +0800

    迭代器模式

--- src/iteratorPattern/index.js
+++ src/iteratorPattern/index.js
@@ -32,13 +32,37 @@ class Container {
 }
 
 //测试 
-let arr = [1,2,3,4,5];
+let arr = [1, 2, 3, 4, 5];
 let container = new Container(arr);
 let result = container.getIterator();
 
-while(result.hasNext()){
+while (result.hasNext()) {
     console.log(result.next());
 };
 
 //场景
 // es6 的 iterator 语法
+
+
+// es6 有序集合的数据类型
+// array map set string TypedArray arguments NodeList
+// 需要有一个统一的遍历接口来遍历所有数据类型
+// object 不是有序集合，可以用 map 代替
+
+// es6 iterator 是什么
+// 以上数据类型，都有 [Symbol.iterator] 属性
+// 属性值是函数，执行函数返回一个迭代器
+// 这个迭代器就有 next 方法可顺序迭代子元素
+// 可运行 Array.prototype[Symbol.iterator] 来测试
+
+function each(data) {
+    let iterator = data[Symbol.iterator]();
+    let item = { done: false };
+    while (!item.done) {
+        item = iterator.next();
+        if (!item.done) {
+            console.log(item.value);
+        }
+    }
+}
+// for ... of 遍历迭代器
