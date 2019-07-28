9775047245742e950c726bf66269a8c6091dd0f0
maoguotao <maoguotao@agree.com.cn>
2019-07-28 16:41:53 +0800

    :zap: 添加vue

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import { getSingle } from "@/singletonPattern/index1.js"
+import Vue from '@/prototype/index'
 
 let fn = function () {
     return '单体模式'
@@ -9,3 +10,12 @@ let fn1 = function () {
 let single = getSingle(fn)
 let single1 = getSingle(fn1)
 console.log('==11=', single(), single() === single())
+
+Vue.prototype.init = function () {
+    let vm = this
+    console.log('vm', vm)
+}
+
+let vue  = new Vue ()
+
+vue.init()
