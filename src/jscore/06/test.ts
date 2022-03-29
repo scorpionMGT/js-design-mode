d1953291914cd34036fd1cc5cb374a11d5702c8b
maoguotao <v_guotao.mao@fowo.com>
2022-03-29 18:29:52 +0800

    更新

--- src/jscore/06/test.ts
+++ src/jscore/06/test.ts
@@ -16,6 +16,7 @@ const Fn = function () {
 
 const fn = new Fn();
 
+console.log('fn', fn.constructor === Fn)
 // console.log('prototype', Fn.__proto__.constructor === Function)
 
 // console.log('prototype', Fn.__proto__.__proto__ === Object.prototype)
@@ -30,7 +31,7 @@ const fn = new Fn();
 
 // console.log('prototype', Fn.prototype.__proto__ === Function.prototype)
 
-// console.log('prototype', Object.prototype.constructor === Object)
+// console.log('prototype', Object.prototype.constructor === Function)
 
 // console.log('prototype', Fn.prototype.__proto__.__proto__)
 
@@ -58,7 +59,36 @@ const fn = new Fn();
 
 // console.log('fn', fn.__proto__.__proto__.__proto__)
 
+// function newFactory () {
+//     let obj = new Object()
+//     // 创建一个新的空对象
+//     let context = Array.prototype.shift.call(arguments)
+//     console.log('arguments', [...arguments], context)
+//     // 空对象的隐式原型指向构造函数原型
+//     obj.__proto__ = context.prototype
+//     // 将空对象的指向构造函数的this, 即将构造函数的内部this, 改为你创建的obj
+//     let result = context.apply(obj, arguments)
+//     // 如果构造函数有返回一个对象，则直接返回这个对象
+//     if (typeof result === 'object' && result !== null) return result
+//     return obj
+// }
+// const Fn1 = function () {
 
+// }
+// let fn1 = newFactory(Fn1);
+
+
+class Person {
+    static className = 'person';
+    static say () {
+        console.log('say', this === Person)
+    }
+    static walk () {
+        console.log('walk', this === Person)
+    }
+}
+
+console.log('person', Person.className, Person.say(), Person.walk())
 
 
 
