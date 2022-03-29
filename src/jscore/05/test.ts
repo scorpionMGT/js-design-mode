a34aa0f2639946ac98a6e4b607ca7facf5c3b39d
maoguotao <v_guotao.mao@fowo.com>
2022-03-29 09:12:51 +0800

    push

--- src/jscore/05/test.ts
+++ src/jscore/05/test.ts
@@ -1,10 +1,37 @@
 // let date = new Date()
 // console.log(date, Date.parse(date))
 
-let regexp = /\[bc\]at/i
+// const test = 'agaat'
+// let regexp = /.at/ig
 
-console.log('ignoreCase', regexp.ignoreCase)
+// console.log('ignoreCase', regexp.ignoreCase)
 
-console.log(regexp.global, 'global')
+// console.log(regexp.global, 'global')
 
-console.log(regexp.source, 'source')
+// console.log(regexp.source, 'source')
+
+// console.log(regexp.exec(test), 'test')
+// console.log(test.match(regexp), 'test')
+
+// let str = new Object('test')
+
+// console.log(str instanceof String)
+
+class Fn {
+    * [Symbol.iterator] () {
+        yield 'test1';
+        yield 2;
+        yield 3;
+    }
+}
+
+const fn = new Fn();
+
+// console.log('fn', fn[Symbol.iterator]().next());
+
+for (const i of fn[Symbol.iterator]()) {
+    console.log('i', i)
+}
+
+const str = 'test'
+console.log('test', str[Symbol.iterator]())
